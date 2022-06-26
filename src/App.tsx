e2d781a5222164bc7d974fe23cb5a7eb4e71758d
kaito-aai <kaito.s0508@gmail.com>
2022-06-26 19:32:12 +0900

    検索機能を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { Movie, MovieTile } from './components/MovieTile';
 import './App.css';
-import { getPopularMovies } from './movie-fetcher';
+import { searchMoviesWithWord, getPopularMovies } from './movie-fetcher';
 
 function App() {
-  const [movies, setMovie] = useState<Movie[]>([])
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [searchWord, setSearchWord] = useState<string>("");
 
+  // mount時
   useEffect(() => {
-    getPopularMovies().then(v => {
-      setMovie(v);
+    getPopularMovies().then(movies => {
+      setMovies(movies);
     })
+    return
   }, [])
+
+  // 検索ワード変更時
+  useEffect(() => {
+    if (!searchWord) {
+      // 検索ワードがないなら人気映画を表示
+      getPopularMovies().then(movies => {
+        setMovies(movies);
+      });
+      return
+    }
+    searchMoviesWithWord(searchWord).then(movies => {
+      setMovies(movies)
+      return
+    })
+  }, [searchWord])
+
+  const onSearchInputChanged = (event: any) => {
+    setSearchWord(event.target.value);
+  }
 
   return (
     <div className="App">
+      <input type="text" name="" id="" value={searchWord} onChange={onSearchInputChanged}/>
       {movies.map((movie, index) => {
         return <MovieTile key={index} {...movie}></MovieTile>
       })}
--- src/movie-fetcher.ts
+++ src/movie-fetcher.ts
@@ -3,17 +3,31 @@ import { Movie } from "./components/MovieTile";
 import { API_KEY, MOVIE_API_URL, MOVIE_IMAGE_URL } from "./keys/TMDB-api-keys";
 axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
 
-
 export const getPopularMovies = (): Promise<Movie[]> => {
     return axios.get(`${MOVIE_API_URL}movie/popular?api_key=${API_KEY}`)
-    .then(res => {
-        const movies: Movie[] = [];
-        res.data.results.forEach((movie: any) => {
-            movies.push({ title: movie.title, description: movie.overview, image: MOVIE_IMAGE_URL+movie.poster_path })
-        });
-        return movies;
-    })
-    .catch(err => {
-        throw err;
-    })
+        .then(res => {
+            const movies: Movie[] = [];
+            res.data.results.forEach((movie: any) => {
+                movies.push({ title: movie.title, description: movie.overview, image: MOVIE_IMAGE_URL+movie.poster_path })
+            });
+            return movies;
+        })
+        .catch(err => {
+            throw err;
+        })
+}
+
+export const searchMoviesWithWord = (word: string): Promise<Movie[]> => {
+    const query = encodeURI(word);
+    return axios.get(`${MOVIE_API_URL}search/movie?api_key=${API_KEY}&query=${query}`)
+        .then(res => {
+            const movies: Movie[] = [];
+            res.data.results.forEach((movie: any) => {
+                movies.push({ title: movie.title, description: movie.overview, image: MOVIE_IMAGE_URL+movie.poster_path })
+            });
+            return movies;
+        })
+        .catch(err => {
+            throw err;
+        })
 }
