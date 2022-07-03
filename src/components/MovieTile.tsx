c084960929307f258cbf7ffe32e5933016da5d87
kaito-aai <kaito.s0508@gmail.com>
2022-07-04 00:02:11 +0900

    descriptionを開閉できるようにした

--- src/components/MovieTile.tsx
+++ src/components/MovieTile.tsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
 import movieTileStyles from "./movieTile.module.scss";
 
 export function MovieTile(movie: Movie) {
+    const { t } = useTranslation();
+    const [isShowModal, setIsShowModal] = useState(false);
+    let localIsShowModal = false
+
+    const changeModal = () => {
+        setIsShowModal(prev => {
+            localIsShowModal = !prev;
+            return !prev;
+        })
+    }
+
     return (
-        <div className={movieTileStyles.movieTile}>
+        <div className={movieTileStyles.movieTile} onClick={() => {changeModal()}}>
             <p className={movieTileStyles.title}>{movie.title}</p>
-            <p className={movieTileStyles.description}>{movie.description}</p>
             <img className={movieTileStyles.image} src={movie.image} alt="" />
+            <div id="overlay" style={{display: isShowModal ? '' : 'none'}}>
+                <div id="content">
+                    <p>{movie.description}</p>
+                </div>
+            </div>
         </div>
     );
 }
